e3e4a3f9ab09a4819337e6d10b328d772dfbffc9
Celia Kelly <celiackelly@gmail.com>
2022-07-10 11:41:15 -0400

    Add comments

--- main.js
+++ main.js
@@ -6,10 +6,12 @@ submitCommentBtn.addEventListener('click', async e => {
             method: 'post', 
             headers: {'Content-Type': 'application/json'}, 
             body: JSON.stringify({
-                name: document.querySelector('input[name="name"]'), 
-                email: document.querySelector('input[name="email"]'), 
-                text: document.querySelector('input[name="text"]'),
-                notificationPreference: document.querySelector('input[name="notifyMe"]')
+                name: document.querySelector('input[name="name"]').value, 
+                email: document.querySelector('input[name="email"]').value, 
+                text: document.querySelector('input[name="text"]').value,
+                //not sure why this is going into the db as "on" instead of "true"
+                notificationPreference: document.querySelector('input[name="notifyMe"]').value
+                //add date/time collection 
             })
         })
         if (response.ok) {
